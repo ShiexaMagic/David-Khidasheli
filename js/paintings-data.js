a132ce4b39a87967ffd3b45d2781519c87da75b4
ShiexaMagic <144231340+ShiexaMagic@users.noreply.github.com>
2026-03-14 18:16:02 +0400

    Update gallery data from admin panel

--- js/paintings-data.js
+++ js/paintings-data.js
@@ -7,7 +7,7 @@ const PaintingsDB = (function () {
     'use strict';
 
     const STORAGE_KEY = 'paintings_db';
-    const DATA_VERSION = 54; // Bump forces full reset of localStorage data
+    const DATA_VERSION = 55; // Bump forces full reset of localStorage data
 
     const defaultPaintings = [
         {
@@ -3053,8 +3053,8 @@ const PaintingsDB = (function () {
         {
             id: 'p243',
             img: 'images/2022_SHI03822.jpg',
-            titleEn: "Painting 2022-234",
-            titleKa: "ნახატი 2022-234",
+            titleEn: "Woman In Love",
+            titleKa: "შეყვარებული ქალი",
             detailEn: "",
             detailKa: "",
             category: 'for-sale',
@@ -3069,8 +3069,8 @@ const PaintingsDB = (function () {
         {
             id: 'p244',
             img: 'images/2022_SHI03827.jpg',
-            titleEn: "Painting 2022-235",
-            titleKa: "ნახატი 2022-235",
+            titleEn: "Woman In Love 2",
+            titleKa: "შეყვარებული ქალი 2",
             detailEn: "",
             detailKa: "",
             category: 'for-sale',
@@ -7167,7 +7167,7 @@ const PaintingsDB = (function () {
 
     // Built-in categories that always exist
     const builtInCategories = [
-        { id: 'for-sale', en: 'For Sale', ka: 'იყიდება', builtin: true },
+        { id: 'for-sale', en: 'For Sale', ka: '\u10d2\u10d0\u10e1\u10d0\u10e7\u10d8\u10d3\u10d8', builtin: true },
         { id: '2021',     en: '2021',     ka: '2021',     builtin: true },
         { id: '2022',     en: '2022',     ka: '2022',     builtin: true },
         { id: '2023',     en: '2023',     ka: '2023',     builtin: true },
